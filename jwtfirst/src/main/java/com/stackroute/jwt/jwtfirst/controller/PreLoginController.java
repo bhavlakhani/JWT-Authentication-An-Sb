543e222d45dcb2694ee0be152c835ca370e6c0d3
saurabhsk <saurabhkumar4634@gmail.com>
2019-03-25 19:00:30 +0530

    Authentication page using angular and spring boot

---
+++ jwtfirst/src/main/java/com/stackroute/jwt/jwtfirst/controller/PreLoginController.java
@@ -0,0 +1,33 @@
+package com.stackroute.jwt.jwtfirst.controller;
+
+
+import com.stackroute.jwt.jwtfirst.domain.Response;
+import com.stackroute.jwt.jwtfirst.model.User;
+import com.stackroute.jwt.jwtfirst.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PreLoginController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping(value = "/registration")
+    public ResponseEntity<Response> registration(@RequestBody User user) {
+        User dbUser = userService.save(user);
+        if (dbUser != null) {
+            return new ResponseEntity<Response>(new Response("User is saved successfully"), HttpStatus.OK);
+        }
+        return null;
+    }
+
+}
+
+
+
